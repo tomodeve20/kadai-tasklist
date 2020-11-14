5b96a899139920d1e418310a60a776410ced25a2
tomo.nitta <tomonitta20@gmail.com>
2020-11-14 04:55:20 +0000

    user pages

---
+++ database/migrations/2020_11_14_134741_add_user_id_to_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->string('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+             $table->dropForeign('tasks_user_id_foreign');
+        });
+    }
+}
